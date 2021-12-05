71c209df9511da9df5bfd125c652b7ab416c99b7
fhs <frederik@hoergreen.dk>
2021-12-05 13:34:54 +0100

    added installation of snapd and spotify

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,5 @@
+set -e
+
 sudo pacman -S \
     lightdm \
     lightdm-gtk-greeter \
@@ -16,9 +18,26 @@ sudo pacman -S \
     jq \
     git \
     code \
-    imwheel
+    imwheel \
     --noconfirm
 
 sudo systemctl enable lightdm
 
 echo "Xcursor.size: 24" > ~/.Xresources
+
+# Install snapd
+git clone https://aur.archlinux.org/snapd.git ~/snapd || echo "snapd already cloned"
+(cd ~/snapd && makepkg -s --noconfirm) || ERROR_CODE=$?
+if [ "${ERROR_CODE}" -ne 13 ]; then
+    echo "something bad happened"
+    exit "${ERROR_CODE}"
+fi
+
+(cd ~/snapd && sudo pacman -U snapd-*-x86_64.pkg.tar.zst --noconfirm)
+
+sudo systemctl enable snapd
+
+# Install spotify
+sudo snap install spotify
+
+echo "Done, now reboot for snapd to work"
